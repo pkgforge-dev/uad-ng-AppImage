ead2f967845dbc4aea927695a85680eb20b8bc8f
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-19 10:44:39 -0400

    fix sed

--- uad-ng-appimage.sh
+++ uad-ng-appimage.sh
@@ -24,7 +24,7 @@ mkdir -p ./AppDir/etc/udev/rules.d
 wget --retry-connrefused --tries=30 "$UDEV" -O ./AppDir/etc/udev/rules.d/51-android.rules
 
 # We also need to be added to a group after installing udev rules
-sed -i '/cp -v '$UDEVDIR'/a	 groupadd -f adbusers; usermod -a -G adbusers $(logname)' ./AppDir/bin/udev-installer.hook
+sed -i '/cp -v \'$UDEVDIR\'/a	 groupadd -f adbusers; usermod -a -G adbusers $(logname)' ./AppDir/bin/udev-installer.hook
 
 # MAKE APPIMAGE WITH URUNTIME
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
